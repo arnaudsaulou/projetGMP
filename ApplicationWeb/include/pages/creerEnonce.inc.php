edbc0e4a3aa9b72f3783a46dbeaac114bd48bdeb
Arnaud SAULOU <a.saulou@gmail.com>
2018-12-09 22:53:40 +0100

    Possiblité d'ajouter un titre, une zone de texte, une donnée variable (coté interface, non fonctionnel)

--- ApplicationWeb/include/pages/creerEnonce.inc.php
+++ ApplicationWeb/include/pages/creerEnonce.inc.php
@@ -7,6 +7,12 @@
            <li><a name="Zone de texte" class="item" id="itemZoneTexte">Zone de texte</a></li>
         </ul>
      </li>
+     <li><a>Données Variable</a>
+        <ul>
+           <li><a name="DonneesVariable" class="item" id="itemDonneeVariable">Donnée Variable</a></li>
+           <li><a name="" class="item" id="">Lien sous menu 1</a></li>
+        </ul>
+     </li>
       <li><a>Question</a>
         <ul>
            <li><a name="Question" class="item" id="itemQuestion">Question</a></li>
@@ -42,13 +48,59 @@
       <h4 id="titreParametrage"></h4>
     </div>
 
-    <label>Valeur :</label>
-    <input id="itemValeur" type="text"></input>
-    <p><p>
-    <p><p>
-    <p><p>
-    <p><p>
-    <p><p>
+    <div id="blockParametrageText">
+
+      <label>Valeur :</label>
+      <input id="itemValeur" type="text"></input>
+
+      <label>Police :</label>
+      <select id="itemPolice">
+        <option value="small">Très petit</option>
+        <option value="medium">Petit</option>
+        <option value="large">Normal</option>
+        <option value="x-large">Grand</option>
+        <option value="xx-large">Très grand</option>
+      </select>
+
+      <label>Couleur :</label>
+      <select id="itemCouleur">
+        <option value="black">Noir</option>
+        <option value="blue">Bleu</option>
+        <option value="red">Rouge</option>
+        <option value="green">Vert</option>
+      </select>
+
+      <input id="itemGras" type="checkbox" value="bold"> <label>Gras</label>
+      <input id="itemItalique" type="checkbox" value="italic"> <label>Italique</label>
+      <input id="itemSousligne" type="checkbox" value="underline"> <label>Souligné</label>
+    </div>
+
+    <div id="blockParametrageDonneeVariable">
+      <input id="itemTypeDonneeValeurAValeur" onclick="typeDonnerClick();" type="radio" name="typeDonnee" checked="checked"> <label>Valeur par valeur</label>
+      <input id="itemTypeDonneeInterval" onclick="typeDonnerClick();"  type="radio" name="typeDonnee"> <label>Interval</label>
+
+      <div id="blockParametrageValeurAValeur">
+        <label>Valeur :</label>
+        <input type="text" id="valeurXDonneeVariable"></input>
+
+        <button>Ajouter une valeur</button>
+
+      </div>
+
+      <div id="blockParametrageInterval">
+          <label>Borne inférieure :</label>
+          <input type="number" id="borneInferieurInterval"></input>
+
+          <label>Borne supérieure :</label>
+          <input type="number" id="borneSuperieurInterval"></input>
+
+          <label>Pas :</label>
+          <input type="number" id="pasInterval"></input>
+      </div>
+
+
+
+    </div>
 
     <button id="bouttonAjouter">Ajouter</button>
   </div>
--- ApplicationWeb/js/creerEnoncer.js
+++ ApplicationWeb/js/creerEnoncer.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
 
-    var textarea = document.getElementById("textarea");
+    var blockParametrageText = document.getElementById("blockParametrageText");
+    var blockParametrageDonneeVariable = document.getElementById("blockParametrageDonneeVariable");
+
+
+
+
     var bouttonAjouter = document.getElementById("bouttonAjouter");
     var itemEnCoursDeCration;
 
@@ -8,6 +13,20 @@ $(document).ready(function() {
     $('.item').click(function(event){
             document.getElementById("titreParametrage").innerHTML = event.target.getAttribute("name");
             itemEnCoursDeCration = event.target;
+
+
+            if(event.target.getAttribute("id") == "itemTitre" || event.target.getAttribute("id") == "itemZoneTexte"){
+              blockParametrageDonneeVariable.style.display  = "none";
+              blockParametrageText.style.display  = "block";
+            }
+
+            if(event.target.getAttribute("id") == "itemDonneeVariable"){
+              blockParametrageText.style.display  = "none";
+              blockParametrageDonneeVariable.style.display  = "block";
+
+              typeDonnerClick();
+
+            }
         });
 
     //Au clique sur le boutton, ajouter l'item à la zone de création
@@ -15,25 +34,82 @@ $(document).ready(function() {
 
 });
 
+function typeDonnerClick() {
+  var radioValaurParValeur =  document.getElementById("itemTypeDonneeValeurAValeur");
+  var radioInterval =  document.getElementById("itemTypeDonneeInterval");
+
+  var blockParametrageValeurAValeur = document.getElementById("blockParametrageValeurAValeur");
+  var blockParametrageInterval = document.getElementById("blockParametrageInterval");
+
+  if(radioValaurParValeur.checked){
+    blockParametrageInterval.style.display  = "none";
+    blockParametrageValeurAValeur.style.display  = "block";
+  } else if(radioInterval.checked){
+    blockParametrageValeurAValeur.style.display  = "none";
+    blockParametrageInterval.style.display  = "block";
+  }
+}
+
 function ajouterElement(typeItem) {
 
   var para1 = document.querySelector('.page_creation');
 
   var itemTitre = typeItem.getAttribute("id");
   var itemValeur = document.getElementById("itemValeur").value;
+  var itemPolice = document.getElementById("itemPolice");
+  itemPolice = itemPolice.options[itemPolice.selectedIndex].value;
+  var itemCouleur = document.getElementById("itemCouleur");
+  itemCouleur = itemCouleur.options[itemCouleur.selectedIndex].value;
+  var itemGras = document.getElementById("itemGras");
+  var itemItalique = document.getElementById("itemItalique");
+  var itemSousligne = document.getElementById("itemSousligne");
+
+  if(itemGras.checked){
+    itemGras = itemGras.value;
+  }
+
+  if(itemItalique.checked){
+    itemItalique = itemItalique.value;
+  }
+
+  if(itemSousligne.checked){
+    itemSousligne = itemSousligne.value;
+  }
 
   switch (itemTitre) {
     case "itemTitre":
         var newTitre = document.createElement('h1');
         newTitre.id = 'titre';
+        newTitre.style.fontSize = itemPolice;
+        newTitre.style.color = itemCouleur;
+        newTitre.style.fontWeight = itemGras;
+        newTitre.style.fontStyle = itemItalique;
+        newTitre.style.textDecoration = itemSousligne;
         newTitre.appendChild(document.createTextNode(itemValeur));
       break;
 
     case "itemZoneTexte":
         var newTitre = document.createElement('p');
         newTitre.id = 'zonedetext';
+        newTitre.style.fontSize = itemPolice;
+        newTitre.style.color = itemCouleur;
+        newTitre.style.fontWeight = itemGras;
+        newTitre.style.fontStyle = itemItalique;
+        newTitre.style.textDecoration = itemSousligne;
         newTitre.appendChild(document.createTextNode(itemValeur));
       break;
+
+      case "itemDonneeVariable":
+          var newTitre = document.createElement('p');
+          newTitre.id = 'donneeVariable';
+          newTitre.style.fontSize = itemPolice;
+          newTitre.style.color = itemCouleur;
+          newTitre.style.fontWeight = itemGras;
+          newTitre.style.fontStyle = itemItalique;
+          newTitre.style.textDecoration = itemSousligne;
+          newTitre.appendChild(document.createTextNode('##1'));
+        break;
+
     default:
       console.log("cc");
   }
