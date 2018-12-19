587598d2ed6fbb61c85b5b640a09a1178269c4d2
BastLast <bastien.last87@gmail.com>
2018-12-19 19:19:41 +0100

    Ajout de quelques commentaires

--- ApplicationWeb/classes/AttribueManager.class.php
+++ ApplicationWeb/classes/AttribueManager.class.php
@@ -6,37 +6,39 @@ class AttribueManager{
 	public function __construct($db){
 		$this->db = $db;
 	}
-	
+
+//cette fonction permet d'enregistrer un attribue dans la base de données
 	public function addAttribue($Attribue){
 		$req=$this->db->prepare
 		('INSERT INTO attribue (idUtilisateur,idSujet,dateAttribution,dateLimite)
 		VALUES (:idUtilisateur,:idSujet,:dateAttribution,:dateLimite)');
-		
+
 		$req->bindValue(':idUtilisateur',$Attribue->getIdUtilisateur(),PDO::PARAM_STR);
 		$req->bindValue(':idSujet',$Attribue->getIdSujet(),PDO::PARAM_STR);
 		$req->bindValue(':dateAttribution',$Attribue->getDateAttribution(),PDO::PARAM_STR);
 		$req->bindValue(':dateLimite',$Attribue->getIdUtilisateur(),PDO::PARAM_STR);
-	
+
 		$req->execute();
 	}
+
+	//cette fonction permet de recuperer un attribue à partie d'un id d'utilisateur et d'un id de sujet
 	public function getAttribueById($idUtilisateur,$idSujet){
-		
-		
 			$req = $this->db->prepare(
-			    'SELECT idUtilisateur,idSujet,dateAttribution, dateLimite 
+			    'SELECT idUtilisateur,idSujet,dateAttribution, dateLimite
 				FROM attribue WHERE idUtilisateur= :idUtilisateur && idSujet= :idSujet');
-				
+
 			$req->bindValue(':idUtilisateur',$idUtilisateur,PDO::PARAM_STR);
 			$req->bindValue(':idSujet',$idSujet,PDO::PARAM_STR);
-			
+
 			$req->execute();
-			
-			
+
+
 			$attribue=$req->fetch(PDO::FETCH_OBJ);
-			
+
 			return new Attribue($attribue);
 			$req->closeCursor();
-		
+
 	}
-	
+
+
 }
--- ApplicationWeb/include/pages/afficherEtudiants.inc.php
+++ ApplicationWeb/include/pages/afficherEtudiants.inc.php
@@ -1,26 +1,31 @@
 <h1> Liste des étudiants enregistrés</h1>
 Il y a
-<?php echo $utilisateurManager->countEtudiants();
+<?php
+//affichage du nombre d'étudiant dans la base de données
+echo $utilisateurManager->countEtudiants();
 ?>
- étudiants enregistrés
+étudiants enregistrés
 
-	<table>
-		<tr class="enTete">
-			<th>Numéro</th>
-			<th>Nom</th>
-			<th>Prenom</th>
-      <th>Moyenne</th>
-		</tr>
+<table>
+  <tr class="enTete">
+    <th>Numéro</th>
+    <th>Nom</th>
+    <th>Prenom</th>
+    <th>Moyenne</th>
+  </tr>
 
-	<?php
-	$listeEtudiants =$utilisateurManager->getListEtudiants();
-	foreach ($listeEtudiants as $etudiant){
-		?>
-		<tr>
-			<td><?php echo $etudiant->getIdUtilisateur()?></td>
-			<td><?php echo $etudiant ->getNom()?></td>
-			<td><?php echo $etudiant ->getPrenom()?></td>
+  <?php
+  //on récupère la liste des étudiants enregistrés
+  $listeEtudiants =$utilisateurManager->getListEtudiants();
+  foreach ($listeEtudiants as $etudiant){
+    ?>
+    <tr>
+      <td><?php echo $etudiant->getIdUtilisateur()?></td>
+      <td><?php echo $etudiant ->getNom()?></td>
+      <td><?php echo $etudiant ->getPrenom()?></td>
       <td><?php echo $utilisateurManager ->calculerMoyenne($etudiant)?></td>
-		</tr>
-	<?php } ?>
-	</table>
+    </tr>
+    <?php
+  }
+  ?>
+</table>
