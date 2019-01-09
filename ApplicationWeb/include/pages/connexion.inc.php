a580e3d9fbe27c5aa18a4d55df00bb2880dcd560
BastLast <bastien.last87@gmail.com>
2019-01-09 18:22:53 +0100

    BugFix mineur

--- ApplicationWeb/include/pages/connexion.inc.php
+++ ApplicationWeb/include/pages/connexion.inc.php
@@ -45,11 +45,8 @@
     $utilisateur = $utilisateurManager->getUtilisateurByLogin($_POST['login']);
     if($utilisateur != null){
       if($utilisateur -> checkPassword($_POST['password'])){
+        
         // le mot de passe est OK
-        $nature = array(
-          1  => "prof",
-          0 => "etu"
-        );
         $_SESSION['co'] = $utilisateur->getPrenom() . " " . $utilisateur->getNom();
         $_SESSION['droits'] = $utilisateur->getEstProf();
         $connexion_en_cours = true;
