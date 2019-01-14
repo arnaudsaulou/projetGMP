19b7c1cca3ffc9540d71c05a9acc35d98823acff
Formula-9 <maxime.barraud1@etu.unilim.fr>
2019-01-14 15:19:44 +0100

    Further improvements to overall system stability and other minor adjustments have been made to enhance the user experience

--- ApplicationWeb/classes/UtilisateurManager.class.php
+++ ApplicationWeb/classes/UtilisateurManager.class.php
@@ -35,7 +35,8 @@ class UtilisateurManager
      * Supprime l'instance d'Utilisateur ayant l'id spécifié.
      * @param integer $id L'ID représentant l'Utilisateur à supprimer.
      */
-    public function supprimerUtilisateurAvecId($id) {
+    public function supprimerUtilisateurAvecId($id)
+    {
         $req = $this->db->prepare("DELETE FROM utilisateur WHERE idUtilisateur = :id");
         $req->bindValue(':id', $id, PDO::PARAM_STR);
         $req->execute();
@@ -63,7 +64,8 @@ class UtilisateurManager
      * @param integer $annee La promotion dont on veut récupérer les étudiants.
      * @return array Un tableau contenant toutes les instances d'Utilisateurs appartenant à la promotion $annee.
      */
-    public function recupererPromotionEtudiante($annee) {
+    public function recupererPromotionEtudiante($annee)
+    {
         $req = $this->db->prepare('SELECT idUtilisateur, nom, prenom FROM utilisateur WHERE estProf = 0 AND annee = :annee');
         $req->bindValue(':annee', $annee, PDO::PARAM_STR);
         $req->execute();
@@ -80,7 +82,8 @@ class UtilisateurManager
      * @param integer $annee La promotion dont on veut récupérer le nombre d'étudiants.
      * @return integer Le nombre d'étudiants dans la promotion $annee.
      */
-    public function recupererNbEtudiantsPromotion($annee) {
+    public function recupererNbEtudiantsPromotion($annee)
+    {
         $req = $this->db->prepare("SELECT count(idUtilisateur) AS total FROM utilisateur WHERE annee = :annee AND estProf = 0");
         $req->bindValue(':annee', $annee, PDO::PARAM_STR);
         $req->execute();
@@ -94,7 +97,8 @@ class UtilisateurManager
      * Retourne une liste contenant toutes les années différentes.
      * @return array Un tableau contenant toutes les années différentes.
      */
-    public function getListeAnnees() {
+    public function getListeAnnees()
+    {
         $req = $this->db->prepare("SELECT DISTINCT annee FROM utilisateur");
         $req->execute();
         $listeAnnee = array();
@@ -102,42 +106,6 @@ class UtilisateurManager
             $listeAnnee[] = $annee;
         }
         return $listeAnnee;
-    }
-
-    /**
-     * Retourne un tableau contenant tous les élèves.
-     * @return array Un tableau contenant tous les élèves.
-     */
-    public function getListEtudiantsAnnee1()
-    {
-        $req = $this->db->prepare('SELECT idUtilisateur,estProf,nom,prenom FROM utilisateur WHERE estProf = 0 AND annee = 1 ORDER BY nom');
-        $req->execute();
-
-        $listeUtilisateur = array();
-
-        while ($Utilisateur = $req->fetch(PDO::FETCH_OBJ)) {
-            $listeUtilisateur[] = new Utilisateur($Utilisateur);
-        }
-        $req->closeCursor();
-        return $listeUtilisateur;
-    }
-
-    /**
-     * Retourne un tableau contenant tous les élèves de l'année 2.
-     * @return array Un tableau contenant tous les élèves.
-     */
-    public function getListEtudiantsAnnee2()
-    {
-        $req = $this->db->prepare('SELECT idUtilisateur,estProf,nom,prenom FROM utilisateur WHERE estProf = 0 AND annee = 2 ORDER BY nom');
-        $req->execute();
-
-        $listeUtilisateur = array();
-
-        while ($Utilisateur = $req->fetch(PDO::FETCH_OBJ)) {
-            $listeUtilisateur[] = new Utilisateur($Utilisateur);
-        }
-        $req->closeCursor();
-        return $listeUtilisateur;
     }
 
     /**
