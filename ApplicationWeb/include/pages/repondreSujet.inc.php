1ab743f87e83d13019da51fad131871240cf2f0f
Formula-9 <maxime.barraud1@etu.unilim.fr>
2019-01-15 15:09:37 +0100

    RepondreSujet -> Support de l'envoi des réponses

--- ApplicationWeb/include/pages/repondreSujet.inc.php
+++ ApplicationWeb/include/pages/repondreSujet.inc.php
@@ -1,7 +1,41 @@
 <?php
+//Récupération de l'Attribue.
 $idEtudiant = $utilisateurManager->getUtilisateurByLogin($_SESSION['log'])->getIdUtilisateur();
 $attribue = $attribueManager->getAttribuePourEtudiant($idEtudiant);
+
+//Récupération de l'Enonce.
 $idSujet = $attribue[0]->getIdSujet();
 $idEnonce = $sujetManager->getSujetAvecId($idSujet)->getIdEnonce();
-$enonce = $enonceManager->recupererEnonceViaIdEnonce($idEnonce);
-echo $enonce->getEnonce();
+$enonce = $enonceManager->recupererEnonceViaIdEnonce($idEnonce)->getEnonce();
+
+//Récupération des données variables
+$listeDonneeVariable = $sujetPossibleManager->recuperListeDonneeVariableViaIdSujet($idSujet);
+
+if (empty($_POST)) { ?>
+
+    <form id="formReponseEnonce" name="formReponseEnonce" action="#" method="post">
+        <?php
+            //Substitution et affichage de l'énoncé.
+            foreach ($listeDonneeVariable as $donneeVariable) {
+                $typeDonnee = $typeDonneeManager->getTypeDonneeById($donneeVariable->getIdType());
+                $enonce = str_replace($typeDonnee->getLibelle(), $donneeVariable->getValeur(), $enonce);
+            }
+            echo $enonce;
+        ?>
+        <input type="submit" value="Envoyer les réponses">
+    </form>
+
+<?php } else {
+    //Stocker les réponses.
+    foreach($_POST as $key => $value) {
+        $numero_question = str_replace('question_', '', $key);
+        $reponseQuestion = new Reponse([
+            'idUtilisateur' => $idEtudiant,
+            'idSujet' => $idSujet,
+            'numReponse' => $numero_question,
+            'valeur' => $value,
+            'dateReponse' => date('Y-m-d')
+        ]);
+        $reponseManager->enregistrerReponse($reponseQuestion);
+    }
+}?>
