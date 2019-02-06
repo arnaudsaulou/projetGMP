956aa542756a8618911861a27dff81feb400e708
BastLast <bastien.last87@gmail.com>
2019-02-07 00:36:07 +0100

    ajout botstrap imprimer sujet

--- ApplicationWeb/include/pages/imprimerSujet.inc.php
+++ ApplicationWeb/include/pages/imprimerSujet.inc.php
@@ -11,31 +11,32 @@
 <script src="packages/pdf/html2pdf.js"></script>
 <script src="js/genererPdf.js"></script>
 <?php
-
 if (isset($_POST['idSujet'])) {
-    $idSujet = $_POST['idSujet'];
-    $attribue = $attribueManager->getAttribueByIdSujet($idSujet);
-    $sujet = $sujetManager->getSujetAvecId($idSujet);
-    $eleve = $utilisateurManager->getUtilisateurById($attribue->getIdUtilisateur());
-    $enonce = str_replace("\n","\\n", $enonceManager->recupererEnonceViaIdEnonce($sujet->getIdEnonce())->getEnonce());
-?>
-
-<script type="text/javascript">
-    var header = '<div class="border col-md-11 mx-auto mt-3"><h5 style="text-decoration: underline;">Nom: <?php echo $eleve->getNom()?> - Prénom: <?php echo $eleve->getPrenom()?> - ID Sujet: <?php echo $idSujet ?></h5>';
-    var footer = '<div class="row fixed-row-bottom mx-auto"></div>';
-    var enonce = header + '<?php echo $enonce ?>' + footer + '</div>';
-    var nom = "Sujet" + "<?php echo $eleve->getNom()?>" + "<?php echo $eleve->getPrenom()?>" + "<?php echo $idSujet ?>";
-    genererPDFAvecHTML(supprimerInputs(enonce), nom);
+  $idSujet = $_POST['idSujet'];
+  $attribue = $attribueManager->getAttribueByIdSujet($idSujet);
+  $sujet = $sujetManager->getSujetAvecId($idSujet);
+  $eleve = $utilisateurManager->getUtilisateurById($attribue->getIdUtilisateur());
+  $enonce = str_replace("\n","\\n", $enonceManager->recupererEnonceViaIdEnonce($sujet->getIdEnonce())->getEnonce());
+  ?>
+  <script type="text/javascript">
+  var header = '<h5 style="text-decoration: underline;">Nom: <?php echo $eleve->getNom()?> - Prénom: <?php echo $eleve->getPrenom()?> - ID Sujet: <?php echo $idSujet ?></h5>';
+  var enonce = header + '<?php echo $enonce ?>';
+  var nom = "Sujet" + "<?php echo $eleve->getNom()?>" + "<?php echo $eleve->getPrenom()?>" + "<?php echo $idSujet ?>";
+  genererPDFAvecHTML(supprimerInputs(enonce), nom);
 </script>
-
 <?php
 } else { ?>
-    <form target="_blank" action="#" method="post">
-        <h3>Insérez l'ID d'un sujet dans le champ ci-dessous pour en générer le PDF: </h3>
-        <label for="idSujet">ID du sujet à imprimer: </label>
-        <input id="idSujet" type="number" name="idSujet" required>
-        <input type="submit" value="Générer PDF">
-    </form>
-<?php
+  <form target="_blank" action="#" method="post">
+    <div class="row mx-auto col-11">
+      <div class="form-group col-7">
+        <label class="col-form-label" for="idSujet">ID du sujet à imprimer: </label>
+        <input class="col-9 col-form-input" id="idSujet" type="number" name="idSujet" required>
+      </div>
+      <div class="form-group col-5">
+        <input  class="col-12 col-form-input btn btn-primary" type="submit" value="Générer PDF">
+      </div>
+    </div>
+  </form>
+  <?php
 }
 ?>
