aeed7357519c93e68e5727f7aeb4fe65fa330bb1
BastLast <bastien.last87@gmail.com>
2019-02-06 19:05:15 +0100

    Petits tweaks divers + update du menu coté prof

--- ApplicationWeb/include/pages/afficherAttributionSujet.inc.php
+++ ApplicationWeb/include/pages/afficherAttributionSujet.inc.php
@@ -21,7 +21,7 @@
                       <th>Date attribution</th>
                       <th>Date limite</th>
                       <th>Temps entre chaque réponse</th>
-                      <th>Détail de la promotion</th>
+                      <th>Afficher les réponses reçues</th>
                   </tr>
                 </thead>
                 <tfoot>
@@ -31,7 +31,7 @@
                       <th>Date attribution</th>
                       <th>Date limite</th>
                       <th>Temps entre chaque réponse</th>
-                      <th>Détail de la promotion</th>
+                      <th>Afficher les réponses reçues</th>
                     </tr>
                 </tfoot>
                 <tbody>
