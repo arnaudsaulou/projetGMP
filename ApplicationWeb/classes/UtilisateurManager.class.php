fe108ea48832117cb7fbf28c2270625d44302bac
BastLast <bastien.last87@gmail.com>
2019-03-04 22:47:59 +0100

    Ajout du reset de password des étudiants par le prof

--- ApplicationWeb/classes/UtilisateurManager.class.php
+++ ApplicationWeb/classes/UtilisateurManager.class.php
@@ -2,267 +2,280 @@
 
 class UtilisateurManager
 {
-    private $db;
-
-    /**
-     * Retourne une nouvelle instance de UtilisateurManager.
-     * @param MyPDO $db Une instance de MyPDO.
-     */
-    public function __construct($db)
-    {
-        $this->db = $db;
-    }
-
-    /**
-     * Enregistre un Utilisateur dans la base de données.
-     * @param Utilisateur $Utilisateur L'utilisateur à enregistrer.
-     */
-    public function addUtilisateur($Utilisateur)
-    {
-        $req = $this->db->prepare
-        ('INSERT INTO utilisateur (estProf,nom,prenom,nomUtilisateur,motDePasse, annee)
-        VALUES (:estProf,:nom,:prenom,:login,:pwd,:annee)');
-        $req->bindValue(':estProf', $Utilisateur->getEstProf(), PDO::PARAM_STR);
-        $req->bindValue(':nom', $Utilisateur->getNom(), PDO::PARAM_STR);
-        $req->bindValue(':prenom', $Utilisateur->getPrenom(), PDO::PARAM_STR);
-        $req->bindValue(':login', $Utilisateur->getNomUtilisateur(), PDO::PARAM_STR);
-        $req->bindValue(':pwd', $Utilisateur->getMotDePasse(), PDO::PARAM_STR);
-        $req->bindValue(':annee', $Utilisateur->getAnnee(), PDO::PARAM_STR);
-        $req->execute();
-        $req->closeCursor();
-    }
-
-    /**
-     * Supprime l'instance d'Utilisateur ayant l'id spécifié.
-     * @param integer $id L'ID représentant l'Utilisateur à supprimer.
-     */
-    public function supprimerUtilisateurAvecId($id)
-    {
-        $req = $this->db->prepare("DELETE FROM utilisateur WHERE idUtilisateur = :id");
-        $req->bindValue(':id', $id, PDO::PARAM_STR);
-        $req->execute();
-        $req->closeCursor();
-    }
-
-    /**
-     * Retourne un tableau contenant tous les utilisateurs du système (professeurs & élèves).
-     * @return array Un tableau contenant tous les utilisateurs du système (professeurs & élèves).
-     */
-    public function getList()
-    {
-        $req = $this->db->prepare('SELECT idUtilisateur,estProf,nom,prenom FROM utilisateur ORDER BY nom');
-        $req->execute();
-        $listeUtilisateur = array();
-        while ($Utilisateur = $req->fetch(PDO::FETCH_OBJ)) {
-            $listeUtilisateur[] = new Utilisateur($Utilisateur);
-        }
-        $req->closeCursor();
-        return $listeUtilisateur;
-    }
-
-    /**
-     * Retourne un tableau contenant toutes les instances d'Utilisateur appartenant à la promotion fournie.
-     * @param integer $annee La promotion dont on veut récupérer les étudiants.
-     * @return array Un tableau contenant toutes les instances d'Utilisateurs appartenant à la promotion $annee.
-     */
-    public function recupererPromotionEtudiante($annee)
-    {
-        $req = $this->db->prepare('SELECT idUtilisateur, nom, prenom FROM utilisateur WHERE estProf = 0 AND annee = :annee');
-        $req->bindValue(':annee', $annee, PDO::PARAM_STR);
-        $req->execute();
-        $listeEtudiants = array();
-        while ($etudiant = $req->fetch(PDO::FETCH_OBJ)) {
-            $listeEtudiants[] = new Utilisateur($etudiant);
-        }
-        $req->closeCursor();
-        return $listeEtudiants;
-    }
-
-    /**
-     * Retourne un tableau contenant toutes les instances d'Utilisateur.
-     * @return array Un tableau contenant toutes les instances d'Utilisateurs.
-     */
-    public function getListEtudiants()
-    {
-        $req = $this->db->prepare('SELECT idUtilisateur, nom, prenom, annee FROM utilisateur WHERE estProf = 0 ');
-        $req->execute();
-        $listeEtudiants = array();
-        while ($etudiant = $req->fetch(PDO::FETCH_OBJ)) {
-            $listeEtudiants[] = new Utilisateur($etudiant);
-        }
-        $req->closeCursor();
-        return $listeEtudiants;
-    }
-
-    /**
-     * Récupère le nombre d'étudiants dans une promotion donnée.
-     * @param integer $annee La promotion dont on veut récupérer le nombre d'étudiants.
-     * @return integer Le nombre d'étudiants dans la promotion $annee.
-     */
-    public function recupererNbEtudiantsPromotion($annee)
-    {
-        $req = $this->db->prepare("SELECT count(idUtilisateur) AS total FROM utilisateur WHERE annee = :annee AND estProf = 0");
-        $req->bindValue(':annee', $annee, PDO::PARAM_STR);
-        $req->execute();
-        $res = $req->fetch(PDO::FETCH_OBJ);
-        $nbUtilisateur = $res->total;
-        $req->closeCursor();
-        return $nbUtilisateur;
-    }
-
-    /**
-     * Retourne une liste contenant toutes les années différentes.
-     * @return array Un tableau contenant toutes les années différentes.
-     */
-    public function getListeAnnees()
-    {
-        $req = $this->db->prepare("SELECT DISTINCT annee FROM utilisateur WHERE annee!=0");
-        $req->execute();
-        $listeAnnee = array();
-        while ($annee = $req->fetch()) {
-            $listeAnnee[] = $annee;
-        }
-        return $listeAnnee;
-    }
-
-    /**
-     * Retourne le nombre d'utilisateurs enregistrés dans la base de données.
-     * @return integer Le nombre d'utilisateurs enregistrés dans la base de données.
-     */
-    public function countUtilisateurs()
-    {
-        $req = $this->db->prepare("SELECT count(idUtilisateur) AS total FROM utilisateur");
-        $req->execute();
-        $res = $req->fetch(PDO::FETCH_OBJ);
-        $nbUtilisateur = $res->total;
-        $req->closeCursor();
-        return $nbUtilisateur;
-    }
-
-    /**
-     * Retourne le nombre d'étudiants enregistrés dans la base de données.
-     * @return integer Le nombre d'étudiants enregistrés dans la base de données.
-     */
-    public function countEtudiants()
-    {
-        $req = $this->db->prepare("SELECT count(idUtilisateur) AS total FROM utilisateur WHERE estProf = 0");
-        $req->execute();
-        $res = $req->fetch(PDO::FETCH_OBJ);
-        $nbUtilisateur = $res->total;
-        $req->closeCursor();
-        return $nbUtilisateur;
-    }
-
-    /**
-     * @param string $login Le nom d'utilisateur de l'Utilisateur à récupérer.
-     * @return Utilisateur|null L'instance d'Utilisateur liée au login passé en paramètre, ou null s'il n'existe pas.
-     */
-    public function getUtilisateurByLogin($login)
-    {
-        $req = $this->db->prepare
-        ("SELECT idUtilisateur,estProf,nom,prenom,nomUtilisateur,motDePasse  FROM utilisateur WHERE nomUtilisateur = :login");
-        $req->bindValue(':login', $login, PDO::PARAM_STR);
-        $req->execute();
-        $resu = $req->fetch(PDO::FETCH_OBJ);
-        $req->closeCursor();
-        if ($resu != null) {
-            return new Utilisateur($resu);
-        } else {
-            return null;
-        }
-    }
-
-    /**
-     * Determine si un Utilisateur est un etudiant à partir de son ID.
-     * @param integer $id L'ID de l'Utilisateur à contrôler.
-     * @return mixed
-     */
-    public function estEtudiantById($id)
-    {
-        $req = $this->db->prepare(
-            'SELECT idUtilisateur FROM etudiant WHERE idUtilisateur = :id'
-        );
-        $req->bindValue(':id', $id, PDO::PARAM_STR);
-        $req->execute();
-        $res = $req->fetch(PDO::FETCH_OBJ);
-        $req->closeCursor();
-        return $res;
-    }
-
-    /**
-     * Retourne une instance d'Utilisateur à partir de son ID.
-     * @param integer $id L'ID de l'Utilisateur à récupérer.
-     * @return Utilisateur L'instance d'Utilisateur associé à cet ID.
-     */
-    public function getUtilisateurById($id)
-    {
-        $req = $this->db->prepare(
-            'SELECT idUtilisateur,estProf,nom, prenom, nomUtilisateur, motDePasse FROM utilisateur WHERE idUtilisateur= :id'
-        );
-        $req->bindValue(':id', $id, PDO::PARAM_STR);
-        $req->execute();
-        $res = $req->fetch(PDO::FETCH_OBJ);
-        $req->closeCursor();
-        return new Utilisateur($res);
-    }
-
-    /**
-     * Calcule la moyenne d'un Etudiant.
-     * @param Utilisateur $etudiant L'etudiant dont on souhaite récupérer la moyenne.
-     * @return double La moyenne d'un Etudiant.
-     */
-    public function calculerMoyenne($etudiant)
-    {
-        $id = $etudiant->getIdUtilisateur();
-        $req = $this->db->prepare(
-            'SELECT AVG(note) AS moyenne FROM note WHERE idUtilisateur = :id'
-        );
-        $req->bindValue(':id', $id, PDO::PARAM_STR);
-        $req->execute();
-        $res = $req->fetch(PDO::FETCH_OBJ);
-        $moyenne = $res->moyenne;
-        $req->closeCursor();
-        return $moyenne;
-    }
-
-	public function changerMotDePasse($nouvMDP,$id){
-
-		$req=$this->db->prepare(
-			'UPDATE utilisateur SET motDePasse=:nouvMDP WHERE idUtilisateur=:id'
-		);
-		$req->bindvalue(':nouvMDP',$nouvMDP,PDO::PARAM_INT);
-		$req->bindValue(':id', $id, PDO::PARAM_STR);
-        $req->execute();
-		$req->closeCursor();
-	}
+  private $db;
 
   /**
-   * Retourne un tableau contenant les statistiques d'une promo d'élève par rapport à un énoncé .
-   * @param int $annee L'année de l'Utilisateur à contrôler.
-   * @param int $Enonce L'ID de l'enonce à contrôler.
-   * @return array Un tableau contenant les statistiques d'une promo d'élève par rapport à un énoncé.
-   */
+  * Retourne une nouvelle instance de UtilisateurManager.
+  * @param MyPDO $db Une instance de MyPDO.
+  */
+  public function __construct($db)
+  {
+    $this->db = $db;
+  }
+
+  /**
+  * Enregistre un Utilisateur dans la base de données.
+  * @param Utilisateur $Utilisateur L'utilisateur à enregistrer.
+  */
+  public function addUtilisateur($Utilisateur)
+  {
+    $req = $this->db->prepare
+    ('INSERT INTO utilisateur (estProf,nom,prenom,nomUtilisateur,motDePasse, annee)
+    VALUES (:estProf,:nom,:prenom,:login,:pwd,:annee)');
+    $req->bindValue(':estProf', $Utilisateur->getEstProf(), PDO::PARAM_STR);
+    $req->bindValue(':nom', $Utilisateur->getNom(), PDO::PARAM_STR);
+    $req->bindValue(':prenom', $Utilisateur->getPrenom(), PDO::PARAM_STR);
+    $req->bindValue(':login', $Utilisateur->getNomUtilisateur(), PDO::PARAM_STR);
+    $req->bindValue(':pwd', $Utilisateur->getMotDePasse(), PDO::PARAM_STR);
+    $req->bindValue(':annee', $Utilisateur->getAnnee(), PDO::PARAM_STR);
+    $req->execute();
+    $req->closeCursor();
+  }
+
+  /**
+  * Supprime l'instance d'Utilisateur ayant l'id spécifié.
+  * @param integer $id L'ID représentant l'Utilisateur à supprimer.
+  */
+  public function supprimerUtilisateurAvecId($id)
+  {
+    $req = $this->db->prepare("DELETE FROM utilisateur WHERE idUtilisateur = :id");
+    $req->bindValue(':id', $id, PDO::PARAM_STR);
+    $req->execute();
+    $req->closeCursor();
+  }
+
+  /**
+  * Retourne un tableau contenant tous les utilisateurs du système (professeurs & élèves).
+  * @return array Un tableau contenant tous les utilisateurs du système (professeurs & élèves).
+  */
+  public function getList()
+  {
+    $req = $this->db->prepare('SELECT idUtilisateur,estProf,nom,prenom FROM utilisateur ORDER BY nom');
+    $req->execute();
+    $listeUtilisateur = array();
+    while ($Utilisateur = $req->fetch(PDO::FETCH_OBJ)) {
+      $listeUtilisateur[] = new Utilisateur($Utilisateur);
+    }
+    $req->closeCursor();
+    return $listeUtilisateur;
+  }
+
+  /**
+  * Retourne un tableau contenant toutes les instances d'Utilisateur appartenant à la promotion fournie.
+  * @param integer $annee La promotion dont on veut récupérer les étudiants.
+  * @return array Un tableau contenant toutes les instances d'Utilisateurs appartenant à la promotion $annee.
+  */
+  public function recupererPromotionEtudiante($annee)
+  {
+    $req = $this->db->prepare('SELECT idUtilisateur, nom, prenom FROM utilisateur WHERE estProf = 0 AND annee = :annee');
+    $req->bindValue(':annee', $annee, PDO::PARAM_STR);
+    $req->execute();
+    $listeEtudiants = array();
+    while ($etudiant = $req->fetch(PDO::FETCH_OBJ)) {
+      $listeEtudiants[] = new Utilisateur($etudiant);
+    }
+    $req->closeCursor();
+    return $listeEtudiants;
+  }
+
+  /**
+  * Retourne un tableau contenant toutes les instances d'Utilisateur.
+  * @return array Un tableau contenant toutes les instances d'Utilisateurs.
+  */
+  public function getListEtudiants()
+  {
+    $req = $this->db->prepare('SELECT idUtilisateur, nom, prenom, annee FROM utilisateur WHERE estProf = 0 ');
+    $req->execute();
+    $listeEtudiants = array();
+    while ($etudiant = $req->fetch(PDO::FETCH_OBJ)) {
+      $listeEtudiants[] = new Utilisateur($etudiant);
+    }
+    $req->closeCursor();
+    return $listeEtudiants;
+  }
+
+  /**
+  * Récupère le nombre d'étudiants dans une promotion donnée.
+  * @param integer $annee La promotion dont on veut récupérer le nombre d'étudiants.
+  * @return integer Le nombre d'étudiants dans la promotion $annee.
+  */
+  public function recupererNbEtudiantsPromotion($annee)
+  {
+    $req = $this->db->prepare("SELECT count(idUtilisateur) AS total FROM utilisateur WHERE annee = :annee AND estProf = 0");
+    $req->bindValue(':annee', $annee, PDO::PARAM_STR);
+    $req->execute();
+    $res = $req->fetch(PDO::FETCH_OBJ);
+    $nbUtilisateur = $res->total;
+    $req->closeCursor();
+    return $nbUtilisateur;
+  }
+
+  /**
+  * Retourne une liste contenant toutes les années différentes.
+  * @return array Un tableau contenant toutes les années différentes.
+  */
+  public function getListeAnnees()
+  {
+    $req = $this->db->prepare("SELECT DISTINCT annee FROM utilisateur WHERE annee!=0");
+    $req->execute();
+    $listeAnnee = array();
+    while ($annee = $req->fetch()) {
+      $listeAnnee[] = $annee;
+    }
+    return $listeAnnee;
+  }
+
+  /**
+  * Retourne le nombre d'utilisateurs enregistrés dans la base de données.
+  * @return integer Le nombre d'utilisateurs enregistrés dans la base de données.
+  */
+  public function countUtilisateurs()
+  {
+    $req = $this->db->prepare("SELECT count(idUtilisateur) AS total FROM utilisateur");
+    $req->execute();
+    $res = $req->fetch(PDO::FETCH_OBJ);
+    $nbUtilisateur = $res->total;
+    $req->closeCursor();
+    return $nbUtilisateur;
+  }
+
+  /**
+  * Retourne le nombre d'étudiants enregistrés dans la base de données.
+  * @return integer Le nombre d'étudiants enregistrés dans la base de données.
+  */
+  public function countEtudiants()
+  {
+    $req = $this->db->prepare("SELECT count(idUtilisateur) AS total FROM utilisateur WHERE estProf = 0");
+    $req->execute();
+    $res = $req->fetch(PDO::FETCH_OBJ);
+    $nbUtilisateur = $res->total;
+    $req->closeCursor();
+    return $nbUtilisateur;
+  }
+
+  /**
+  * @param string $login Le nom d'utilisateur de l'Utilisateur à récupérer.
+  * @return Utilisateur|null L'instance d'Utilisateur liée au login passé en paramètre, ou null s'il n'existe pas.
+  */
+  public function getUtilisateurByLogin($login)
+  {
+    $req = $this->db->prepare
+    ("SELECT idUtilisateur,estProf,nom,prenom,nomUtilisateur,motDePasse  FROM utilisateur WHERE nomUtilisateur = :login");
+    $req->bindValue(':login', $login, PDO::PARAM_STR);
+    $req->execute();
+    $resu = $req->fetch(PDO::FETCH_OBJ);
+    $req->closeCursor();
+    if ($resu != null) {
+      return new Utilisateur($resu);
+    } else {
+      return null;
+    }
+  }
+
+  /**
+  * Determine si un Utilisateur est un etudiant à partir de son ID.
+  * @param integer $id L'ID de l'Utilisateur à contrôler.
+  * @return mixed
+  */
+  public function estEtudiantById($id)
+  {
+    $req = $this->db->prepare(
+      'SELECT idUtilisateur FROM etudiant WHERE idUtilisateur = :id'
+    );
+    $req->bindValue(':id', $id, PDO::PARAM_STR);
+    $req->execute();
+    $res = $req->fetch(PDO::FETCH_OBJ);
+    $req->closeCursor();
+    return $res;
+  }
+
+  /**
+  * Retourne une instance d'Utilisateur à partir de son ID.
+  * @param integer $id L'ID de l'Utilisateur à récupérer.
+  * @return Utilisateur L'instance d'Utilisateur associé à cet ID.
+  */
+  public function getUtilisateurById($id)
+  {
+    $req = $this->db->prepare(
+      'SELECT idUtilisateur,estProf,nom, prenom, nomUtilisateur, motDePasse FROM utilisateur WHERE idUtilisateur= :id'
+    );
+    $req->bindValue(':id', $id, PDO::PARAM_STR);
+    $req->execute();
+    $res = $req->fetch(PDO::FETCH_OBJ);
+    $req->closeCursor();
+    return new Utilisateur($res);
+  }
+
+  /**
+  * Calcule la moyenne d'un Etudiant.
+  * @param Utilisateur $etudiant L'etudiant dont on souhaite récupérer la moyenne.
+  * @return double La moyenne d'un Etudiant.
+  */
+  public function calculerMoyenne($etudiant)
+  {
+    $id = $etudiant->getIdUtilisateur();
+    $req = $this->db->prepare(
+      'SELECT AVG(note) AS moyenne FROM note WHERE idUtilisateur = :id'
+    );
+    $req->bindValue(':id', $id, PDO::PARAM_STR);
+    $req->execute();
+    $res = $req->fetch(PDO::FETCH_OBJ);
+    $moyenne = $res->moyenne;
+    $req->closeCursor();
+    return $moyenne;
+  }
+
+  /**
+  * Permet de modifier le mot de passe d'un utilisateur
+  * @param string $nouvMDP le nouveau mot de passe choisi
+  * @param int $id L'ID de l'utilisateur dont le mot de passe doit être changé
+  */
+  public function changerMotDePasse($nouvMDP,$id){
+    $req=$this->db->prepare(
+      'UPDATE utilisateur SET motDePasse=:nouvMDP WHERE idUtilisateur=:id'
+    );
+    $req->bindvalue(':nouvMDP',$nouvMDP,PDO::PARAM_STR);
+    $req->bindValue(':id', $id, PDO::PARAM_STR);
+    $req->execute();
+    $req->closeCursor();
+  }
+
+  /**
+  * Permet de réinitialiser le mot de passe d'un utilisateur au mot de passe par defaut
+  * @param int $id L'ID de l'utilisateur dont le mot de passe doit être changé
+  */
+  public function resetMdp($id){
+    $mdp = "password";
+    $this->changerMotDePasse($mdp,$id);
+  }
+
+  /**
+  * Retourne un tableau contenant les statistiques d'une promo d'élève par rapport à un énoncé .
+  * @param int $annee L'année de l'Utilisateur à contrôler.
+  * @param int $Enonce L'ID de l'enonce à contrôler.
+  * @return array Un tableau contenant les statistiques d'une promo d'élève par rapport à un énoncé.
+  */
   public function getStatReponseEtudiantParAnneeEtEnonce($annee, $Enonce)
   {
     $req = $this->db->prepare(
-                      'SELECT distinct nom, prenom, COUNT( DISTINCT r.dateReponse) as nbReponses, MAX(note) as meilleureNote,  MIN(r.dateReponse) as premiereRep, MAX(r.dateReponse) as derniereRep
-                       FROM utilisateur u
-                       JOIN attribue a ON a.idUtilisateur = u.idUtilisateur
-                       JOIN sujet s ON s.idSujet = a.idSujet
-                       JOIN note n ON n.idUtilisateur = u.idUtilisateur
-                       JOIN reponses r ON r.idUtilisateur = a.idUtilisateur
-                       WHERE annee = :annee AND s.idEnonce = :Enonce
-                      ');
-    $req->bindValue(':annee', $annee, PDO::PARAM_STR);
-    $req->bindValue(':Enonce', $Enonce, PDO::PARAM_STR);
-    $req->execute();
+      'SELECT distinct nom, prenom, COUNT( DISTINCT r.dateReponse) as nbReponses, MAX(note) as meilleureNote,  MIN(r.dateReponse) as premiereRep, MAX(r.dateReponse) as derniereRep
+      FROM utilisateur u
+      JOIN attribue a ON a.idUtilisateur = u.idUtilisateur
+      JOIN sujet s ON s.idSujet = a.idSujet
+      JOIN note n ON n.idUtilisateur = u.idUtilisateur
+      JOIN reponses r ON r.idUtilisateur = a.idUtilisateur
+      WHERE annee = :annee AND s.idEnonce = :Enonce
+      ');
+      $req->bindValue(':annee', $annee, PDO::PARAM_STR);
+      $req->bindValue(':Enonce', $Enonce, PDO::PARAM_STR);
+      $req->execute();
 
-    $listeStatReponse = array();
-    while ($eleve = $req->fetch(PDO::FETCH_OBJ)) {
+      $listeStatReponse = array();
+      while ($eleve = $req->fetch(PDO::FETCH_OBJ)) {
         $listeStatReponse[] = $eleve;
-    }
-    $req->closeCursor();
-    return $listeStatReponse;
+      }
+      $req->closeCursor();
+      return $listeStatReponse;
 
+    }
   }
-}
--- ApplicationWeb/include/pages/supprimerEtudiants.inc.php
+++ ApplicationWeb/include/pages/supprimerEtudiants.inc.php
@@ -6,12 +6,12 @@
   <li class="breadcrumb-item active">Supprimer un étudiant</li>
 </ol>
 
-<?php if (!isset($_POST['id_etu'])) { ?>
+<?php if (!isset($_POST['id_etumdp']) && !isset($_POST['id_etusupr'])) { ?>
 
   <div class="card mb-3">
     <div class="card-header">
       <i class="fas fa-table"></i>
-    Séléctionnez l'étudiant à supprimer
+      Séléctionnez l'étudiant à supprimer
     </div>
     <div class="card-body">
       <div class="table-responsive">
@@ -21,7 +21,8 @@
               <th>Numéro</th>
               <th>Nom</th>
               <th>Prénom</th>
-              <th>Ann&eacute;e</th>
+              <th>Année</th>
+              <th>Réinitialiser mot de passe ?</th>
               <th>Supprimer ?</th>
             </tr>
           </thead>
@@ -36,7 +37,10 @@
                 <td><?php echo $etudiant->getPrenom() ?></td>
                 <td><?php echo $etudiant->getAnnee() ?></td>
                 <td>
-                  <a href="#" onclick="reglerValeur(<?php echo $etudiant->getIdUtilisateur() ?>);"><i class="fas fa-trash-alt"></i></a>
+                  <a href="#" onclick="changerMDP(<?php echo $etudiant->getIdUtilisateur() ?>);"><i class="fas fa-lock-open"></i></a>
+                </td>
+                <td>
+                  <a href="#" onclick="supprimer(<?php echo $etudiant->getIdUtilisateur() ?>);"><i class="fas fa-trash-alt"></i></a>
                 </td>
               </tr>
               <?php
@@ -47,9 +51,13 @@
       </div>
     </div>
     <script type="text/javascript">
-    function reglerValeur(valeur) {
-      document.formSupprimerEtudiant.id_etu.value = valeur;
+    function supprimer(valeur) {
+      document.formSupprimerEtudiant.id_etusupr.value = valeur;
       $('#supprModal').modal('show');
+    }
+    function changerMDP(valeur) {
+      document.formMdpEtudiant.id_etumdp.value = valeur;
+      $('#mdpModal').modal('show');
     }
     </script>
   </div>
@@ -59,7 +67,7 @@
     <div class="modal-dialog" role="document">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Voulez vous vraiment supprimer l'&eacute;tudiant ?</h5>
+          <h5 class="modal-title" id="exampleModalLabel">Voulez vous vraiment supprimer l'étudiant ?</h5>
           <button class="close" type="button" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">×</span>
           </button>
@@ -67,7 +75,7 @@
         <div class="modal-body">Appuyer sur "Supprimer" pour confirmer.</div>
         <div class="modal-footer">
           <form id="formSupprimerEtudiant" name="formSupprimerEtudiant" method="post" action="#">
-            <input id="id_etu" name="id_etu" type="hidden" value="">
+            <input id="id_etusupr" name="id_etusupr" type="hidden" value="">
             <button class="btn btn-secondary" type="button" data-dismiss="modal">Annuler</button>
             <input class="btn btn-danger" type="submit" value="Supprimer">
           </form>
@@ -76,23 +84,58 @@
     </div>
   </div>
 
-<?php } else {
-  $noteManager->supprimerNotesAvecIdEtudiant($_POST['id_etu']);
-  $reponseManager->supprimerReponsesAvecIdEtudiant($_POST['id_etu']);
-  $attribueManager->supprimerAttribueAvecIdEtudiant($_POST['id_etu']);
-  $utilisateurManager->supprimerUtilisateurAvecId($_POST['id_etu']);
-  ?>
-
-  <div class="suppressionConfirme">
-    <div class='row justify-content-center'>
-      <div class="col-4 align-self-center alert alert-success" role="alert">
-        <p>L'étudiant a bien été supprimé !</p>
+  <!-- Modal reset mdp -->
+  <div class="modal fade" id="mdpModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Voulez vous vraiment réinitialiser le mot de passe de l'étudiant ?</h5>
+          <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">×</span>
+          </button>
+        </div>
+        <div class="modal-body">Le mot de passe de l'étudiant sera défini sur "password"</div>
+        <div class="modal-footer">
+          <form id="formMdpEtudiant" name="formMdpEtudiant" method="post" action="#">
+            <input id="id_etumdp" name="id_etumdp" type="hidden" value="">
+            <button class="btn btn-secondary" type="button" data-dismiss="modal">Annuler</button>
+            <input class="btn btn-danger" type="submit" value="Réinitialiser">
+          </form>
+        </div>
       </div>
     </div>
-    <a class="btn btn-link" href="index.php?page=12"><p>Retour</p></a>
   </div>
 
-<?php } ?>
+<?php } else {
+  if(!isset($_POST['id_etumdp'])){
+    $noteManager->supprimerNotesAvecIdEtudiant($_POST['id_etusupr']);
+    $reponseManager->supprimerReponsesAvecIdEtudiant($_POST['id_etusupr']);
+    $attribueManager->supprimerAttribueAvecIdEtudiant($_POST['id_etusupr']);
+    $utilisateurManager->supprimerUtilisateurAvecId($_POST['id_etusupr']);
+    ?>
+    <div class="suppressionConfirme">
+      <div class='row justify-content-center'>
+        <div class="col-4 align-self-center alert alert-success" role="alert">
+          <p>L'étudiant a bien été supprimé !</p>
+        </div>
+      </div>
+      <a class="btn btn-link" href="index.php?page=12"><p>Retour</p></a>
+    </div>
+
+  <?php } else{
+    $utilisateurManager->resetMdp($_POST['id_etumdp']);
+?>
+<div class="suppressionConfirme">
+  <div class='row justify-content-center'>
+    <div class="col-4 align-self-center alert alert-success" role="alert">
+      <p>Le mot de passe a bien été enregistré !</p>
+    </div>
+  </div>
+  <a class="btn btn-link" href="index.php?page=12"><p>Retour</p></a>
+</div>
+<?php
+  }
+} ?>
 
 <!-- Page level plugin CSS-->
 <link href="packages/datatables/dataTables.bootstrap4.css" rel="stylesheet">
