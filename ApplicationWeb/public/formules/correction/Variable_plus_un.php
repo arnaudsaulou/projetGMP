a4cafd97cab6210cdd36ceba5c5d66043bbb9fce
BastLast <bastien.last87@gmail.com>
2019-03-19 18:39:28 +0100

    Dernier commit pre-soutenance ;)

---
+++ ApplicationWeb/public/formules/correction/Variable_plus_un.php
@@ -0,0 +1,18 @@
+<?php
+
+class Formule {
+
+  public static function Variable_plus_un($prametres){
+    //Extraction des paramètres (ne pas modifier)
+    $intervale = $prametres[0];
+
+    // ------------------------------------ //
+    //  Coder ci-dessous la fonction voulu  //
+    // ------------------------------------ //
+
+    return $intervale + 1;
+  }
+
+}
+
+ ?>
